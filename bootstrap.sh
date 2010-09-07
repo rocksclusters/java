8f8f56fdd767aac46b70d269f6b8db39bdcb36d6
Greg Bruno <greg.bruno@gmail.com>
2010-09-07 23:53:21 +0000

    star power for gb

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 #
 # This file should remain OS independent
 #
-# $Id: bootstrap.sh,v 1.5 2009/05/01 19:07:19 mjk Exp $
+# $Id: bootstrap.sh,v 1.6 2010/09/07 23:53:21 bruno Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		       version 5.2 (Chimichanga)
+# 		         version 5.4 (Maverick)
 # 
-# Copyright (c) 2000 - 2009 The Regents of the University of California.
+# Copyright (c) 2000 - 2010 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -58,6 +58,9 @@
 # @Copyright@
 #
 # $Log: bootstrap.sh,v $
+# Revision 1.6  2010/09/07 23:53:21  bruno
+# star power for gb
+#
 # Revision 1.5  2009/05/01 19:07:19  mjk
 # chimi con queso
 #
