d32d4827b9bf4586eeec63d504c5bca7afc733e8
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-09-08 02:24:24 +0000

    Updated Java for Solaris

--- src/sun-java/version.mk
+++ src/sun-java/version.mk
@@ -1,5 +1,5 @@
 NAME = jdk
 VERSION = 1.6.0
-RELEASE = 13
-X86_TARBALL = jdk-6u13-solaris-i586.tar.Z
-X64_TARBALL = jdk-6u13-solaris-x64.tar.Z
+RELEASE = 27
+X86_TARBALL = jdk-6u27-solaris-i586.tar.Z
+X64_TARBALL = jdk-6u27-solaris-x64.tar.Z
